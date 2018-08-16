37f00bfbd62c152b23781eeaf5a53de55660b4f4
bestlong <bestlong168@gmail.com>
2018-08-16 18:38:50 +0800

    code reformat

--- mssql.js
+++ mssql.js
@@ -5,7 +5,6 @@ module.exports = function(RED) {
 
 	function connection(config) {
 	    RED.nodes.createNode(this, config);
-
         var node = this;
 	    this.config = {
             user: node.credentials.username,
@@ -20,20 +19,17 @@ module.exports = function(RED) {
             }
         };
 
-
-	    this.connection = sql;
-			/*
-			node.on('close',function(){
-   			node.pool.close(function(){});
-   		})
-			*/
-		}
+        this.connection = sql;
+        // node.on('close',function(){
+        //     node.pool.close(function(){});
+        // });
+    }
 
   	RED.nodes.registerType('MSSQL-CN', connection, {
 	    credentials: {
-				username: {type:'text'},
-				password: {type:'password'},
-				domain: {type: 'text'}
+            username: {type:'text'},
+            password: {type:'password'},
+            domain: {type: 'text'}
 	    }
 	});
 
@@ -45,61 +41,57 @@ module.exports = function(RED) {
 	    this.config = mssqlCN.config;
 	    this.outField = config.outField;
 
-      var node = this;
+        var node = this;
 	    var b = node.outField.split('.');
-      var i = 0;
-      var r = null;
-      var m = null;
-      var rec = function(obj) {
-          i += 1;
-          if ((i < b.length) && (typeof obj[b[i-1]] === 'object')) {
-              rec(obj[b[i-1]]); // not there yet - carry on digging
-          }
-          else {
-               if (i === b.length) { // we've finished so assign the value
-                   obj[b[i-1]] = r;
-                   node.send(m);
-                   node.status({});
-               }
-               else {
-                   obj[b[i-1]] = {}; // needs to be a new object so create it
-                   rec(obj[b[i-1]]); // and carry on digging
-               }
-          }
-      };
-
-      node.on('input',function(msg){
-          node.connection.connect(node.config).then(function(){
-
-            node.status({fill:'blue',shape:'dot',text:'requesting'});
-
-            var query = mustache.render(node.query,msg);
-
-            if (!query || (query === '')) {
-                query = msg.payload;
+        var i = 0;
+        var r = null;
+        var m = null;
+        var rec = function(obj) {
+            i += 1;
+            if ((i < b.length) && (typeof obj[b[i-1]] === 'object')) {
+                rec(obj[b[i-1]]); // not there yet - carry on digging
             }
+            else {
+                if (i === b.length) { // we've finished so assign the value
+                    obj[b[i-1]] = r;
+                    node.send(m);
+                    node.status({});
+                }
+                else {
+                    obj[b[i-1]] = {}; // needs to be a new object so create it
+                    rec(obj[b[i-1]]); // and carry on digging
+                }
+            }
+        };
 
-            var request = new node.connection.Request();
+        node.on('input',function(msg){
+            node.connection.connect(node.config).then(function(){
+                node.status({fill:'blue',shape:'dot',text:'requesting'});
 
-            request.query(query).then(function (rows){
-        		i = 0;
-        		r = rows;
-        		m = msg;
-        		rec(msg);
+                var query = mustache.render(node.query,msg);
+
+                if (!query || (query === '')) {
+                    query = msg.payload;
+                }
+
+                var request = new node.connection.Request();
+
+                request.query(query).then(function (rows){
+                    i = 0;
+                    r = rows;
+                    m = msg;
+                    rec(msg);
+                }).catch(function(err) {
+                    node.error(err);
+                    node.status({fill:'red',shape:'ring',text:'Error'});
+                    return;
+                });
             }).catch(function(err) {
                 node.error(err);
                 node.status({fill:'red',shape:'ring',text:'Error'});
                 return;
             });
-
-
-        }).catch(function(err) {
-    		node.error(err);
-    		node.status({fill:'red',shape:'ring',text:'Error'});
-    		return;
         });
-      });
-
 	}
   	RED.nodes.registerType('MSSQL', mssql);
 };
