fdb0aa4fe3384a207d74ab626c163bfa1b1a438f
Shao Yu-Lung (Allen) <bestlong168@gmail.com>
2023-02-10 15:40:13 +0800

    fix: Parse mustache feature default must active. #69

--- src/mssql.js
+++ src/mssql.js
@@ -493,7 +493,7 @@ module.exports = function (RED) {
         node.paramsOptType = config.paramsOptType || 'none';
         node.rows = config.rows || 'rows';
         node.rowsType = config.rowsType || 'msg';
-        node.parseMustache = config.parseMustache;
+        node.parseMustache = config.parseMustache || true;
 
         const setResult = function (msg, field, value, returnType = 0) {
             // eslint-disable-next-line eqeqeq
