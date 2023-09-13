fe9a744568fabe561cf4038520141c26e3f18ca8
Shao Yu-Lung (Allen) <bestlong168@gmail.com>
2023-09-13 20:54:10 +0800

    fix(devops): mssql create testdb fail

--- docker/configure-db.sh
+++ docker/configure-db.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# Wait 60 seconds for SQL Server to start up by ensuring that 
+# Wait 60 seconds for SQL Server to start up by ensuring that
 # calling SQLCMD does not return an error code, which will ensure that sqlcmd is accessible
 # and that system and user databases return "0" which means all databases are in an "online" state
-# https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=sql-server-2017 
+# https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=sql-server-2017
 
 DBSTATUS=1
 ERRCODE=1
-i=0
 
-while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 60 ]] && [[ $ERRCODE -ne 0 ]]; do
-	i=$i+1
+i=15
+sleep $i
+
+while [[ $i -lt 60 ]] && ( [[ $ERRCODE -ne 0 ]] || [[ $DBSTATUS -ne 0 ]] ); do
 	DBSTATUS=$(/opt/mssql-tools/bin/sqlcmd -h -1 -t 1 -U sa -P $MSSQL_SA_PASSWORD -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases")
 	ERRCODE=$?
+
+	let "DBSTATUS=DBSTATUS+1-1"
+	let "i=i+1"
+	echo "    ### i: $i, ERRCODE: $ERRCODE, DBSTATUS: $DBSTATUS"
+
 	sleep 1
 done
 
-if [ $DBSTATUS -ne 0 ] || [ $ERRCODE -ne 0 ]; then 
+if [ $DBSTATUS -ne 0 ] || [ $ERRCODE -ne 0 ]; then
 	echo "SQL Server took more than 60 seconds to start up or one or more databases are not in an ONLINE state"
 	exit 1
 fi
